bb0f6c588b49739dac38f6c7a2cb807b20d2cec1
matheusmadeira <fumacammadeira@gmail.com>
2024-06-05 21:25:58 -0300

    fix: ajusteDelete

--- src/Middlewares/verificarUsuario.js
+++ src/Middlewares/verificarUsuario.js
@@ -1,16 +1,20 @@
-const  jwt  = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
-function verificarUsuario(req, res, next){
-    const usuarioId = req.params.id_usuario || req.body.id_usuario;
+function verificarUsuario(req, res, next) {
+  const usuarioId = req.params.id_usuario || req.body.id_usuario;
 
-    const authHeader = req.headers.authorization || req.headers.Authorization;
+  const authHeader = req.headers.authorization || req.headers.Authorization;
 
-    const [bearer, token] = authHeader.split(" ");
+  const [bearer, token] = authHeader.split(" ");
 
-    const {usuario} = jwt.decode(token);
-    if(!usuario.nivel && usuario._id !== usuarioId) return res.status(401).json({ message: "Usuário sem permissão!" });
+  const { usuario } = jwt.decode(token);
+  console.log(req.params);
+  console.log(usuario._id);
 
-    next();
+  if (!usuario.nivel && usuario._id !== usuarioId)
+    return res.status(401).json({ message: "Usuário sem permissão!" });
+
+  next();
 }
 
 module.exports = verificarUsuario;
